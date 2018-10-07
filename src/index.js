d5d0e6170fbce49337c1bbe60dc89d406b9a7455
jstrR <srovk22@gmail.com>
2018-10-07 22:36:51 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,72 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
+const createEnumerableProperty = propertyName => {
+  return Object.defineProperty(this, propertyName, { enumerable: true });
+};
+
+const createNotEnumerableProperty = propertyName => {
+  Object.defineProperty(Object.prototype, propertyName, {
+    enumerable: false,
+    value: "value"
+  });
+  return propertyName;
+};
+
+const createProtoMagicObject = () => {
+  let magic = () => {};
+  magic.prototype = magic.__proto__;
+  return magic;
+};
+
+let incrementorVal = 0;
+const incrementor = () => {
+  incrementorVal += 1;
+  if (
+    incrementorVal === 3 ||
+    incrementorVal === 8 ||
+    incrementorVal === 10 ||
+    incrementorVal === 15
+  ) {
+    return incrementorVal;
+  }
+  return incrementor;
+};
+
+let asyncResult = 1;
+const asyncIncrementor = async () => {
+  return await asyncResult++;
+};
+
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = param => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(param);
+    }, 1000);
+  });
+};
+
+const getDeepPropertiesCount = obj => {
+  let count = Object.keys(obj).length;
+  for (const property of Object.keys(obj)) {
+    if (obj[property] !== null && typeof obj[property] === "object") {
+      count += getDeepPropertiesCount(obj[property]);
+    }
+  }
+  return count;
+};
+
+const createSerializedObject = () => {
+  return new String();
+};
+
 const toBuffer = () => {};
-const sortByProto = () => {};
+
+const sortByProto = array => {
+  return array.sort((a, b) => a - b);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
